b984995b64371e44b901b20b04051f9e111698ee
AriHildan <arihildan2004@gmail.com>
2020-12-04 14:21:02 +0700

    update

--- app/src/main/java/com/example/pas_11rpl1_arihildan/Login.java
+++ app/src/main/java/com/example/pas_11rpl1_arihildan/Login.java
@@ -1,50 +1,47 @@
 package com.example.pas_11rpl1_arihildan;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+
     SharedPreferences pref;
     SharedPreferences.Editor editor;
 
-    EditText user, password;
+    EditText txtusername;
+    EditText txtpassword;
     Button btnlogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         pref = getSharedPreferences("login", MODE_PRIVATE);
-        user = findViewById(R.id.user);
-        password = findViewById(R.id.password);
-        btnlogin = findViewById(R.id.btnlogin);
-
-
+        txtusername = (EditText)findViewById(R.id.user);
+        txtpassword = (EditText)findViewById(R.id.password);
+        btnlogin = (Button) findViewById(R.id.btnlogin);
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String mUser = user.getText().toString();
-                String mPassword = password.getText().toString();
-
-                if (mUser.equalsIgnoreCase("salah") && mPassword.equalsIgnoreCase("salah")){
-                    Toast.makeText(Login.this,"Anda salah", Toast.LENGTH_LONG).show();
-                }else {
+            public void onClick(View view) {
+                if (txtusername.getText().toString().equalsIgnoreCase("admin")
+                        && txtpassword.getText().toString().equalsIgnoreCase("admin")){
+                    //saving ke SP
                     editor = pref.edit();
-                    editor.putString("username", mUser);
+                    editor.putString("username", txtusername.getText().toString());
                     editor.putString("status", "login");
                     editor.apply();
-
+                    //menuju ke main menu
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     finish();
                 }
-
             }
         });
     }
